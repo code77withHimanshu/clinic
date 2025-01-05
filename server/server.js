5d3257bb5d505b04f9a63076df14f77cc770b755
Himanshu <hsinghi3@gmail.com>
2025-01-05 20:46:51 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://test:test123@clinic-appointment.62kve.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
